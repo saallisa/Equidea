57e7c6cf366f9008221a3adeee77a58e4a45bae2
Lisa Saalfrank <lsaalfrank@hof-university.de>
2018-02-10 12:35:04 +0100

    Add render method to AbstractBaseController

--- src/Bridge/Controller/AbstractBaseController.php
+++ src/Bridge/Controller/AbstractBaseController.php
@@ -11,7 +11,7 @@ use Equidea\Core\Utility\Container;
  * @package     Equidea
  * @version     0.1.0-dev
  */
-abstract class AbstractBaseController {
+class AbstractBaseController {
 
     /**
      * @var \Equidea\Core\Http\Interfaces\RequestInterface
@@ -41,5 +41,18 @@ abstract class AbstractBaseController {
         $this->request = $request;
         $this->response = $response;
         $this->container = $container;
+    }
+
+    /**
+     * @param   string  $template
+     * @param   array   $data
+     *
+     * @return  \Equidea\Core\Http\Interfaces\ResponseInterface
+     */
+    protected function render(string $template, array $data) : ResponseInterface
+    {
+        $view = $this->container->retrieve('Template.Engine');
+        $content = $view->render($template, $data);
+        return $this->response->withBody($content);
     }
 }
