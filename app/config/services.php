7fde0b7968fcdb6c0d61f47fe9e651de9f7121d9
Lisa Saalfrank <lsaalfrank@hof-university.de>
2018-02-11 15:12:13 +0100

    Update services.php

--- app/config/services.php
+++ app/config/services.php
@@ -1,6 +1,14 @@
 <?php
 
 // ----------------------------------------------------------------------
+// The Core classes
+// ----------------------------------------------------------------------
+$container->register('Template.Engine', function() use ($container) {
+    $config = $container->getConfig('templating');
+    return new \Equidea\Core\Templating\Engine($config);
+});
+
+// ----------------------------------------------------------------------
 // The Controller classes
 // ----------------------------------------------------------------------
 $container->register('PagesController',
